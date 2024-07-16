0bfc6f04d528dca50d20d656811d79d8e6616330
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2024-07-16 10:27:59 +0200

    add test

--- internal/binaries/node/matches_test.go
+++ internal/binaries/node/matches_test.go
@@ -2,7 +2,10 @@ package node
 
 import (
 	"fmt"
+	"os"
 	"testing"
+
+	"github.com/svenliebig/binary-organizer/internal/shell"
 )
 
 func TestMatcher(t *testing.T) {
@@ -31,4 +34,30 @@ func TestMatcher(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestMatcherWithPath(t *testing.T) {
+	t.Run("should find node 16", func(t *testing.T) {
+		os.Setenv("PATH", "~/workspace/.cache/go/bin:~/workspace/software/bin:~/workspace/software/go/bin:/opt/homebrew/bin:~/workspace/software/jdk-17/Contents/Home/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/Library/Apple/usr/bin:/usr/local/share/dotnet:~/.dotnet/tools:~/workspace/software/node/node-v16.20.2-darwin-arm64/bin")
+
+		p := shell.NewPath()
+
+		b := binary{}
+		nodePaths := p.Find(func(s string) bool {
+			_, matches := b.Matches(s)
+			return matches
+		})
+
+		if len(nodePaths) == 0 {
+			t.Errorf("expected to find node binary")
+		}
+
+		if len(nodePaths) > 1 {
+			t.Errorf("expected to find only one node binary")
+		}
+
+		if nodePaths[0] != "~/workspace/software/node/node-v16.20.2-darwin-arm64/bin" {
+			t.Errorf("expected to find node binary in ~/workspace/software/node/node-v16.20.2-darwin-arm64/bin")
+		}
+	})
 }
