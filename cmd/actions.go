3f014bdcaf3dcdec62478596a575c408bb0e24d8
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2024-08-23 15:20:24 +0200

    docs(cmd): add comments to the annotation variables

--- cmd/actions.go
+++ cmd/actions.go
@@ -2,10 +2,12 @@ package cmd
 
 import "github.com/spf13/cobra"
 
+// if this annotation is used the command will source the $PATH after the command is executed.
 var annotationSourceTrue = map[string]string{
 	"source": "true",
 }
 
+// if this annotation is used the command will not source the $PATH after the command is executed.
 var annotationSourceFalse = map[string]string{
 	"source": "false",
 }
